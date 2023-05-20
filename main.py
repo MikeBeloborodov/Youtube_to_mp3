e7c69e0ea4f2693cd5bddad100075761d11206ca
MikeBeloborodov <kiritohenjanai@gmail.com>
2023-05-20 21:05:45 +0400

    created some basic functionality

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 from fastapi import FastAPI
+import os
+import utils
+
+# create Downloads folder
+if not os.path.exists(utils.PATH_TO_DOWNLOADS):
+    os.mkdir(utils.PATH_TO_DOWNLOADS)
 
 
+# initiate FastAPI app
 app = FastAPI()
 
 
 @app.get("/")
 def home():
     message = "Welcome to youtube to mp3 conerter!"
-    return {"Message: ": message}
+    return message
+
+
+@app.get("/youtube_downloader")
+def youtube_downloader():
+    url = "https://www.youtube.com/watch?v=V66PMeImkxI"
+    file_name = utils.download_youtube_audio(url, utils.PATH_TO_DOWNLOADS)
+    path_to_mp4 = utils.PATH_TO_DOWNLOADS + "/" + file_name + ".mp4"
+    path_to_mp3 = utils.PATH_TO_DOWNLOADS + "/" + file_name + ".mp3"
+    utils.convert_mp4_to_mp3(path_to_mp4, path_to_mp3)
+
+
+@app.get("/delete_files")
+def delete_downloaded_files():
+    utils.delete_files()
---
+++ utils.py
@@ -0,0 +1,49 @@
+import os
+import pathlib
+from moviepy.editor import AudioFileClip
+from pytube import YouTube
+
+
+PATH_TO_DOWNLOADS = str(pathlib.Path(__file__).parent.resolve()) + '\Downloads'
+
+
+def convert_mp4_to_mp3(path_to_mp4: str, path_to_mp3: str) -> None:
+    """
+    Converts mp4 file to mp3 file.
+    Arguments: 
+        path_to_mp4 [str]: Path to mp4 file you want to convert.
+        path_to_mp3 [str]: Path to save converted file in mp3 format.
+    """
+
+    file_to_convert = AudioFileClip(path_to_mp4) 
+    file_to_convert.write_audiofile(path_to_mp3)
+    file_to_convert.close()
+
+
+def download_youtube_audio(url: str, path_to_file:str) -> str:
+    """
+    Downloads an audio file from youtube video. 
+    Arguments:
+        url [str]: A url string from a youtube video.
+        path_to_file [str]: Path for saving the file.
+    Return:
+        file_name [str]: The name of a downloaded file.
+    """
+
+    youtube_video = YouTube(url)
+
+    streams = youtube_video.streams.filter(only_audio=True, file_extension="mp4")
+    best_quality_stream = streams[-1]
+    best_quality_stream.download(output_path=path_to_file)
+    file_name = youtube_video.title
+
+    return file_name
+
+
+def delete_files() -> None:
+    """
+    Deletes all files in Downloads folder.
+    """
+    file_names = os.listdir(PATH_TO_DOWNLOADS)
+    for file in file_names:
+        os.remove(PATH_TO_DOWNLOADS + "/" + file)
